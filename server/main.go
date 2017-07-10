e24c8c2c143fec7d751240a9f84d1a90aea8e2df
aborisov <borisov88@mail.ru>
2017-07-10 15:42:52 +0300

    server 02: add logger

--- server/main.go
+++ server/main.go
@@ -1,39 +1,38 @@
 package main
 
 import (
-	_ "fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
 )
 
 const URL = "http://db.chgk.info/xml/random"
 
+func actionGetQuestions(w http.ResponseWriter, r *http.Request) {
+	c := http.Client{}
+	res, err := c.Get(URL)
+	if err != nil {
+		log.Println(err)
+	}
+	defer res.Body.Close()
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		content, err := ioutil.ReadFile("./public/index.html")
-		if err != nil {
-			log.Println(err)
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
+	r := mux.NewRouter().StrictSlash(true)
+	r.Handle("/", http.FileServer(http.Dir("public"))).Methods(http.MethodGet)
+	r.HandleFunc("/questions", actionGetQuestions).Methods(http.MethodGet)
 
-		w.Write(content)
-	})
+	n := negroni.Classic()
+	n.Use(negroni.NewLogger())
+	n.UseHandler(r)
 
-	http.HandleFunc("/api/quiz/", func(w http.ResponseWriter, r *http.Request) {
-		c := http.Client{}
-		res, err := c.Get(URL)
-		if err != nil {
-			log.Println(err)
-		}
-		defer res.Body.Close()
-
-		body, _ := ioutil.ReadAll(res.Body)
-
-		w.WriteHeader(http.StatusOK)
-		w.Write(body)
-	})
-
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	n.Run(":8081")
 }
