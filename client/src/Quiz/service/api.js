b97a9954c335748dad0d59d0be4635ee868fa117
aborisov <borisov88@mail.ru>
2017-07-30 14:49:34 +0300

    prod build

--- client/src/Quiz/service/api.js
+++ client/src/Quiz/service/api.js
@@ -1,8 +1,11 @@
 // @flow
 
 import axios from 'axios';
+import { isProd } from '../../utils/util';
 
-const URL = 'http://localhost:8081/questions';
+const URL = isProd
+    ? 'https://morning-springs-21420.herokuapp.com/questions'
+    : 'http://localhost:8081/questions';
 
 export function getQuestions() {
     return axios.get(URL);
