a94a6f5804789ab657c16d79aced597f9d974297
aborisov <borisov88@mail.ru>
2017-08-02 09:50:50 +0300

    Redesign style for  Mobile size

--- client/src/Quiz/utils/format.js
+++ client/src/Quiz/utils/format.js
@@ -1,4 +1,5 @@
 export function formatQuestion(data) {
+    console.log(data);
     return data.search.question.map((question) => ({
         id: question.questionid,
         question: question.question,
