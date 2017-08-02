2bc8cd21c05efa24e6da916788055412bb477ab2
aborisov <borisov88@mail.ru>
2017-08-02 10:18:57 +0300

    Replace quote

--- client/src/Quiz/utils/format.js
+++ client/src/Quiz/utils/format.js
@@ -1,9 +1,13 @@
 export function formatQuestion(data) {
-    console.log(data);
     return data.search.question.map((question) => ({
         id: question.questionid,
-        question: question.question,
-        answer: question.answer,
-        comments: typeof question.comments === 'string' ? question.comments : ''
+        question: replaceQuote(question.question),
+        answer: replaceQuote(question.answer),
+        comments: typeof question.comments === 'string' ? replaceQuote(question.comments) : ''
     }));
+}
+
+
+function replaceQuote(str) {
+    return str.replace(/\&quot;/gi, '\"');
 }
